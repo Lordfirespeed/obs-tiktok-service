d432ae86b15c0cc30de87374ef80a16b46d0ec23
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-12-21 04:28:22 +0000

    make the main plugin entrypoint a `.cpp` file

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -18,6 +18,7 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 #include <plugin-support.h>
+#include <httplib.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
